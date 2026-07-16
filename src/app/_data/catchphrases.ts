e21b8d93e97dd15cc3b704fca61c483b57a53d14
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2026-07-15 22:18:35 -0700

    Update catchphrases
    Still the most entertaining drama of the year.

--- src/app/_data/catchphrases.ts
+++ src/app/_data/catchphrases.ts
@@ -1,33 +1,36 @@
 // * src/data/catchphrases.js
 
-const catchphraseData = [
+const catchphraseData : Array<string> = [
   "Still inconsistent, but now with a website!",
-  "TikTok's back, but I still won't use it.",
+  "Never had a TikTok, never will have one.",
   "Never had an Instagram, never will have one.",
   "Definitely NOT procrastinating on something.",
-  "Certified TRON 2.0 Enthusiast.",
-  "Only the most inconsistent content on the web.",
+  "Certified TRON 2.0 Enthusiast",
+  "Only the most inconsistent content on the Internet.",
   "quaaaaaaaaaaaaaaaagsire",
   "Totally not a Cylon agent. Why would you think that?",
-  "Most known for uploading the soundtracks of space games.",
+  "Most known for uploading the soundtracks of video games.",
   "Least known for playing Paladins, God rest its soul.",
   "The most normal patron of the Kaamo Club.",
   "Creator of TRON 2.0: Security Protocol, coming soon!",
   "Developer of ESBuilder, beta available now!",
   "Prefers SiIvaGunner over actual music.",
   "Spent WAY too much time making these subheaders.",
-  "BMC - Street Party - Bloons TD 6",
+  "Electric Tooth Brush (Beta Mix) - Jet Set Radio",
   "Always the designated family I.T. guy.",
   "Kalos enjoyer, Galar disliker.",
-  "Really likes Congo African Grey videos.",
+  "Now an installable web app! Try it out!",
   "Get Quagsire in Smash Bros.",
   "Stop wasting lasers. They're not cheap, you know.",
-  "Azalea Town x Last Christmas. Think about it.",
+  "Blow My Speakers Up! (Blown Out Mix) - Jet Set Radio Evolution",
   "Hoping you have a good day!",
-  "Pokémon Masters EX Quitter (Make Quagsire Playable).",
+  "Pokémon Local Acts Enjoyer",
   "Wishing there were more than 24 hours in a day.",
   "Thinks the Minecraft logo yellowtext is cool.",
-  "Reform Copyright Law.",
+  "Repeal Section 1201 of the DMCA.",
+  "Graphic design is his passion.",
+  "Stop Killing Games.",
+  "Still the most entertaining drama of the year?"
 ];
 
 export default catchphraseData;
